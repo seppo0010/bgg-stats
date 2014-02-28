4cac1bf962e740b1b37c4ecc6994dd7c61ea5609
Sebastian Waisbrot <seppo@simpler.co>
2014-02-27 18:18:10 -0800

    Add header to csvs

--- run.sh
+++ run.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
 node index.js > a.csv
-grep designer a.csv |sort > designer.csv
-grep "game voted" a.csv |sort > "game voted.csv"
-grep "game owned" a.csv |sort > "game owned.csv"
-grep mechanic a.csv |sort > mechanic.csv
-grep user a.csv |sort > user.csv
-grep publisher a.csv |sort > publisher.csv
+echo "designer\tNombre\tUnidades\tRating" > designer.csv
+grep designer a.csv |sort >> designer.csv
+echo "game voted\tNombre\tVotos\tPromedio" > "game voted.csv"
+grep "game voted" a.csv |sort >> "game voted.csv"
+echo "game owned\tNombre\tUnidades" > "game owned.csv"
+grep "game owned" a.csv |sort >> "game owned.csv"
+echo "mechanic\tNombre\tVotos\tPromedio" > mechanic.csv
+grep mechanic a.csv |sort >> mechanic.csv
+echo "user\tNombre\tUnidades\tVotos\tPromedio" > user.csv
+grep user a.csv |sort >> user.csv
+echo "publisher\tNombre\tVotos\tPromedio" > publisher.csv
+grep publisher a.csv |sort >> publisher.csv
